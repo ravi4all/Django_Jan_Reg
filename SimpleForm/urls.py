ab5601a40a90b180021c40ae548c9f33eb1e55d0
ravi4all <ravikanttyagi.2295@gmail.com>
2018-01-07 23:03:50 +0530

    Add files via upload

---
+++ SimpleForm/urls.py
@@ -0,0 +1,8 @@
+# from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(r'', views.index, name='index'),
+    path(r'login/', views.login, name='login')
+]
