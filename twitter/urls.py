b9d879fd8f53867aa82fa9f95f2d3eefefcd6186
ravi4all <ravikanttyagi.2295@gmail.com>
2018-01-30 15:57:49 +0530

    Add files via upload

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 
 app_name = "twitter"
 
 urlpatterns = [
     path(r'', views.index, name="index"),
-    path(r'login/', login, {'template_name' : 'includes/login.html'}, name="login")
+    path(r'register/', views.register, name="register"),
+    path(r'login/', login, {'template_name' : 'includes/login.html'}, name="login"),
+    path(r'logout/', logout, {'template_name' : 'includes/logout.html'}, name="logout")
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(req):
     return render(req, 'index.html')
+
+def register(req):
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+
+        if form.is_valid():
+            form.save()
+            return render(req, '/twitter/')
+
+    else:
+        form = UserCreationForm()
+        context = {'form' : form}
+        return render(req, 'includes/registration.html', context)
