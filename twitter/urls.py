d91c02b22771e7df53f64d972996138e059720f9
ravi4all <ravikanttyagi.2295@gmail.com>
2018-02-09 10:44:22 +0530

    Add files via upload

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import login
-from django.contrib.auth.views import logout
+from django.contrib.auth.views import login, logout
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -13,5 +12,6 @@ urlpatterns = [
     path(r'profile/', views.view_profile, name="profile"),
     path(r'edit_profile/', views.edit_profile, name="edit_profile"),
     path(r'login/', login, {'template_name' : 'includes/login.html'}, name="login"),
-    path(r'logout/', logout, {'template_name' : 'includes/logout.html'}, name="logout")
+    path(r'logout/', logout, {'template_name' : 'includes/logout.html'}, name="logout"),
+    path(r'change_password/', views.change_password, name="change_password"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- twitter/views.py
+++ twitter/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterUser, EditProfile
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(req):
@@ -19,6 +21,7 @@ def register(req):
         context = {'form' : form}
         return render(req, 'includes/registration.html', context)
 
+@login_required
 def view_profile(req):
     user = req.user
 
@@ -26,6 +29,7 @@ def view_profile(req):
 
     return render(req, 'includes/profile.html', context)
 
+@login_required
 def edit_profile(req):
     if req.method == 'POST':
         form = EditProfile(req.POST, instance=req.user)
@@ -37,3 +41,15 @@ def edit_profile(req):
         form = EditProfile(instance=req.user)
         context = {'form' : form}
         return render(req, 'includes/edit_profile.html', context)
+
+@login_required
+def change_password(req):
+    if req.method == 'POST':
+        form = PasswordChangeForm(data=req.POST, user=req.user)
+        if form.is_valid():
+            form.save()
+            return redirect('/twitter/profile')
+    else:
+        form = PasswordChangeForm(user=req.user)
+        context = {'form' : form}
+        return render(req, 'includes/change_password.html', context)
